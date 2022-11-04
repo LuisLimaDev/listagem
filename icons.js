0dab6c68a953ce72f638f5ea8e312d003fca30e0
LuisLimaDev <56639074+LuisLimaDev@users.noreply.github.com>
2022-11-04 12:43:14 -0300

    Update icons.js

--- icons.js
+++ icons.js
@@ -2,12 +2,12 @@
 icones= {
 	importar: ['&#11121;', '&#11145;'],
 	exportar: ['&#11123;', '&#11147;'],
-	delItem: ['&#11199;'],
+	delItem: ['<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="30" width="30" viewBox="0 -2 100 100"><path d="m20, 25 60,0 0,50 -10, 10 -40,0 -10,-10z m-5,-5 70,0 0,-3 -20,0 0,-2 -30,0 0,2 -20,0z"></path></svg>'],
 	atualiza: ['&#11118;'],
 	iconeEstrela: ['&#11088;', '&#128969;', '&#128974;'],
 	soma: ['&#128929;', '&#128930;', '&#128931;', '&#128932;', '&#128933;', '&#128934;', '&#128935;'],
 	multiplica: ['&#128936;', '&#128937;', '&#128938;', '&#128939;', '&#128940;', '&#128941;', '&#128942;'],
-	edita: ['&#9998;'],
+	edita: ['<svg viewBox="0 0 24 24" height="30" width="30"><path fill="currentColor" d="M3.95,16.7v3.4h3.4l9.8-9.9l-3.4-3.4L3.95,16.7z M19.75,7.6c0.4-0.4,0.4-0.9,0-1.3 l-2.1-2.1c-0.4-0.4-0.9-0.4-1.3,0l-1.6,1.6l3.4,3.4L19.75,7.6z"></path></svg>'],
 	recorta: ['&#9986;'],
 	fone: ['&#9990;', '&#9743;', '&#9742;'],
 	mail: ['&#9993;'],
@@ -24,8 +24,9 @@ icones= {
 	livre: ['&#127379;'],
 	novo: ['&#127381;'],
 	confirma: ['&#127383;'],
-	setas: ['&#129128;', '&#129120;' /** esquerda **/ , '&#129122;', '&#129130;' /** direita **/, '&#129129;', '&#129121;' /** acima **/, '&#129131;', '&#129123;' /** abaixo **/ ],
+	setas: ['&#129128;', '<svg viewBox="0 -4 24 24" width="30"><title>Fechar menu principal</title><path fill="currentColor" d="M12,4l1.4,1.4L7.8,11H20v2H7.8l5.6,5.6L12,20l-8-8L12,4z"></path></svg>' /** esquerda **/ , '&#129122;', '&#129130;' /** direita **/, '&#129129;', '&#129121;' /** acima **/, '&#129131;', '&#129123;' /** abaixo **/ ],
 	deletar: ['&#9249;'],
+	download: ['<svg viewBox="0 -2 24 24" width="30" height="15"><path fill="currentColor" d="m12,4 0,12 0,0 0,-12z m-6,7 6,5 6,-5 m-12,8 12,0" style="fill: none;stroke-width: 2px;"></path></svg>'],
 	config: ['&#9965;'],
 	draw: function(forma){
 		return '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="40" height="40"><text fill="rgb(0 0 0)" style="transform: scale(1.5) translate(7px, 20px);">' + forma + '</text></svg>'
